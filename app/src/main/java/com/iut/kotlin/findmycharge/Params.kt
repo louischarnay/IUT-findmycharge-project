6d1dfd6ca9827a64df3ed73228ae6c7e9280c3e5
Louis Charnay <louis.charnay@orange.fr>
2022-03-24 13:38:17 +0100

    adding params activity

---
+++ app/src/main/java/com/iut/kotlin/findmycharge/Params.kt
@@ -0,0 +1,11 @@
+package com.iut.kotlin.findmycharge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Params : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_params)
+    }
+}
